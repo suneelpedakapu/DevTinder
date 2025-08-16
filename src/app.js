7caccb7137a2d1ba6f1fc30a75957ecc3f3b451d
suneelpedakapu <sunilpedakapu143@gmail.com>
2025-08-16 12:14:04 +0530

    Changed BE

---
+++ src/app.js
@@ -0,0 +1,10 @@
+const express=require('express');
+const app=express();
+
+app.use("/test",(req,res)=>{
+    res.send("Hello from the server...");
+});
+
+app.listen(3000,()=>{
+    console.log("My server successfully running on port 3000...")
+});
