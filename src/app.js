68df14cbea9dec6afa353983cb4017edc266691e
suneelpedakapu <sunilpedakapu143@gmail.com>
2025-08-16 20:05:53 +0530

    Used GET,POST,DELETE API calls

--- src/app.js
+++ src/app.js
@@ -1,10 +1,29 @@
 const express=require('express');
 const app=express();
 
-app.use("/test",(req,res)=>{
-    res.send("Hello from the server 3000...");
+
+
+app.get("/users",(req,res)=>{
+    res.send({firstname:"Suneel",lastname:"Pedakapu"})
+})
+
+
+
+app.post("/user",(req,res)=>{
+    res.send("User added successfully to DB")
+})
+
+app.delete("/user",(req,res)=>{
+    res.send("User Deleted successfully from DB")
+})
+
+
+app.use("/",(req,res)=>{
+    res.send("Hello test ");
 });
 
+
+
 app.listen(3000,()=>{
-    console.log("My server successfully running on port 3000...")
+    console.log("My server successfully running on port 3000...");
 });
