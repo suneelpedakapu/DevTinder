03f2ba3ec119842d63e6ef3a29ec5f9ddfa85854
suneelpedakapu <sunilpedakapu143@gmail.com>
2025-08-22 14:58:21 +0530

    update user

--- src/app.js
+++ src/app.js
@@ -6,18 +6,17 @@ const User=require("./models/user");
 
 app.use(express.json()); //Middleware to convert json to js obj
 
-// posting user taking details from body of postman
-// app.post("/signUp",async (req,res)=>{
-//   const user=new User(req.body);
-//   try{
-//     await user.save();
-//     res.send("User saved succcessfully")
-//   }
-//   catch(err){
-//     res.status(401).send("Error addding user")
-//   }
-// });
-
+// API to post user taking details from body of postman
+app.post("/signUp",async (req,res)=>{
+  const user=new User(req.body);
+  try{
+    await user.save();
+    res.send("User saved succcessfully")
+  }
+  catch(err){
+    res.status(401).send("Error addding user")
+  }
+});
 
 // API to find particular user using emailId
 app.get("/users",async (req,res)=>{
@@ -33,7 +32,7 @@ app.get("/users",async (req,res)=>{
 // API call to get all users in User Model database
 app.get("/feed",async(req,res)=>{
   try{
-    const users=await User.find({});
+    const users=await User.findById('68a4ac4cbfdebf3c9ddbe721');
     res.send(users)
   }
   catch(err){
@@ -41,6 +40,31 @@ app.get("/feed",async(req,res)=>{
   }
 });
 
+//API to delete user by Id
+app.delete("/userDelete",async (req,res)=>{
+  const userId=req.body.userId;
+  try{
+    await User.findByIdAndDelete(userId);
+    res.send("User Deleted Successfully");
+  }
+  catch(err){
+    res.status(401).send("Error in deleting User")
+  }
+})
+
+// API to update user details with Email
+app.patch("/userUpdate",async(req,res)=>{
+  const userId=req.body._id;
+  const data=req.body;
+  try{
+    await User.findByIdAndUpdate(userId,data)
+    // await User.findOneAndUpdate({emailId:userId},data);
+    res.send("user updated successfully");
+  }
+  catch(err){
+    res.status(401).send("Error in updating User")
+  }
+})
 
 dbConnect().
 then(()=>{
